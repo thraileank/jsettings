95101fa00d546879e55bd6285956262e8c5662fc
thraileank <cashandito@yandex.ru>
2021-08-07 21:37:04 +0300

    fix indents

--- param/Param.hpp
+++ param/Param.hpp
@@ -9,71 +9,71 @@
 #include "SourceTypes.hpp"
 
 namespace JSettings {
-	//TODO define concept that will restrict possible types
-	template<typename T>
-	class Param {
-		friend class ParamTypeConverter;
-	public:
-		Param() = default;
-		
-		Param(int id, std::string_view name, ValueTypes type, const T& defaultValue)
-			: id_(id)
-			, name_(name)
-			, type_(type) {
-				values_[SourceTypes::DEFAULT] = defaultValue;
-			}
+    //TODO define concept that will restrict possible types
+    template<typename T>
+    class Param {
+        friend class ParamTypeConverter;
+    public:
+        Param() = default;
 
-		Param(int id, std::string_view name, ValueTypes type, const std::map<SourceTypes, T>& values)
-			: id_(id)
-			, name_(name)
-			, type_(type)
-			, values_(values) {}
+        Param(int id, std::string_view name, ValueTypes type, const T& defaultValue)
+            : id_(id)
+            , name_(name)
+            , type_(type) {
+                values_[SourceTypes::DEFAULT] = defaultValue;
+            }
 
-	public:
-		T getValue() const {
-			return values_.rbegin()->second;
-		}
+        Param(int id, std::string_view name, ValueTypes type, const std::map<SourceTypes, T>& values)
+            : id_(id)
+            , name_(name)
+            , type_(type)
+            , values_(values) {}
 
-		T getValue(SourceTypes source) const {
-			return values_.at(source);
-		}
+    public:
+        T getValue() const {
+            return values_.rbegin()->second;
+        }
 
-		void setValue(SourceTypes source, const T& value) {
-			if (source == SourceTypes::DEFAULT) {
-				throw std::invalid_argument("Attempt to update default value!");
-			} else {
-				values_[source] = value;
-			}
-		}
+        T getValue(SourceTypes source) const {
+            return values_.at(source);
+        }
 
-		void deleteValue(SourceTypes source) {
-			if (source == SourceTypes::DEFAULT) {
-				throw std::invalid_argument("Attempt to delete default value!");
-			} else {
-				values_.erase(source);
-			}
-		}
+        void setValue(SourceTypes source, const T& value) {
+            if (source == SourceTypes::DEFAULT) {
+                throw std::invalid_argument("Attempt to update default value!");
+            } else {
+                values_[source] = value;
+            }
+        }
 
-		ValueTypes getType() const {
-			return type_;
-		}
+        void deleteValue(SourceTypes source) {
+            if (source == SourceTypes::DEFAULT) {
+                throw std::invalid_argument("Attempt to delete default value!");
+            } else {
+                values_.erase(source);
+            }
+        }
 
-		void debugPrint() const {
-			std::cout << "==========================\n"
-				<< "SettingsEntity\n"
-				<< "id: " << id_ << "\n"
-				<< "name: " << name_ << "\n"
-				<< "type: " << type_ << "\n"
-				<< "values:\n";
-				for (const auto value : values_) {
-					std::cout << value.first << " " << value.second << "\n";
-				}
-		}
+        ValueTypes getType() const {
+            return type_;
+        }
 
-	private:
-		int id_ = -1;
-		std::string name_;
-		ValueTypes type_;
-		std::map<SourceTypes, T> values_;
-	};
+        void debugPrint() const {
+            std::cout << "==========================\n"
+                << "SettingsEntity\n"
+                << "id: " << id_ << "\n"
+                << "name: " << name_ << "\n"
+                << "type: " << type_ << "\n"
+                << "values:\n";
+            for (const auto value : values_) {
+                std::cout << value.first << " " << value.second << "\n";
+            }
+        }
+
+    private:
+        int id_ = -1;
+        std::string name_;
+        ValueTypes type_;
+        std::map<SourceTypes, T> values_;
+    };
 }
